b2af0a73f328c45fb26406cfe6ff85b0ebcdcc16
Atul Patil <atulpatil539@gmail.com>
2021-10-10 12:35:09 +0530

    feat: matrial ui setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from "./Header";
 
 
 function App() {
   return (
     <div className = "app_wrapper">
-
+      
+         <Header />
 
     </div>
    
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>
+        <div className = "header">
+            <div className = "header__left">
+                <div className = "header__logo">
+                 <img src = "https://cdn-icons.flaticon.com/png/128/3536/premium/3536505.png?token=exp=1633848944~hmac=60c4423e04b9a96275fcd326d6b22469"/>
+                </div>
+                <div className = "header__search">
+
+                </div>
+
+            </div>
+            <div className = "header__right">
+                
+           </div>
             
         </div>
     )
